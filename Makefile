aa363a0e8fba832340dc774ae389ea8395348c1c
arzelcm <arzelcanavate@gmail.com>
2024-02-22 14:53:53 +0100

    Add testing env

---
+++ Makefile
@@ -0,0 +1,9 @@
+all:
+	cc -Wall -Werror -Wextra -g get_next_line_utils.c  get_next_line.c tests/main.c -o main -D BUFFER_SIZE=42
+
+m: all
+	./main
+	
+leaks: all
+	leaks -quiet -fullContent -atExit -- ./main
+
---
+++ tests/main.c
@@ -0,0 +1,25 @@
+#include "../get_next_line.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+int	main(void)
+{
+	char *auxline;
+	// int	fd = 1;
+	int	fd = open("tests/test2.txt", O_RDONLY);
+	char *line = get_next_line(fd);
+	int i = 0;
+
+	while (line)
+	{
+		printf("%-2i: %s", i + 1, line);
+		auxline = get_next_line(fd);
+		free(line);
+		line = auxline;
+		i++;
+	}
+	if (line)
+		free(line);
+	close(fd);
+	return (0);
+}
